f0cbd97fa6914bcb0e3f35927d92f192d0022777
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-06 17:12:06 +0000

    Draft 1 - links dynamically created

--- app.rb
+++ app.rb
@@ -1,9 +1,49 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+require "dotenv/load"
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCH_KEY")}"
+ 
+  @raw_response = HTTP.get(api_url)
+  @raw_string = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@raw_string)
+  @curr = @parsed_response.fetch("currencies")
+  @curr_list = @curr.keys
+
+  
+  #@curr_response = @raw_string.fetch("currencies")
+
+  erb(:homepage)
+  
 end
+
+get("/:from_currency") do
+  
+  @from_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCH_KEY")}"
+ 
+  @raw_response = HTTP.get(api_url)
+  @raw_string = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@raw_string)
+  @curr = @parsed_response.fetch("currencies")
+  @curr_list = @curr.keys
+
+  
+  erb(:from_to_view)
+end
+
+# @curr_list.each do |from|
+#   @from_currency = "/" + from
+#   get(@from_currency) do
+#   end
+
+#   erb(:from_to_view)
+  
+
+# end
