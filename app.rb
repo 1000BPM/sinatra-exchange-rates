41cf4e113907accfccd1cf7009f6f25e4c7a71e6
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-06 17:30:46 +0000

    Convert parsing complete

--- app.rb
+++ app.rb
@@ -38,6 +38,22 @@ get("/:from_currency") do
   erb(:from_to_view)
 end
 
+get("/:from_currency/:to_currency") do
+  "test!!!!"
+  @from_currency = params.fetch("from_currency")
+  @to_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?from=#{@from_currency}&to=#{@to_currency}&amount=1&access_key=#{ENV.fetch("EXCH_KEY")}"
+ 
+  @raw_response = HTTP.get(api_url)
+  @raw_string = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@raw_string)
+  @amt_converted = @parsed_response.fetch("result")
+  
+  erb(:convert_result)
+end
+
 # @curr_list.each do |from|
 #   @from_currency = "/" + from
 #   get(@from_currency) do
